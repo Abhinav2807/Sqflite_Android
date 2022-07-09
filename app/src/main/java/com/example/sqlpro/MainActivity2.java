7a3e6f797e8ecff25565d1375fdb84902633201c
Abhinav <ayush.abhinav1999@gmail.com>
2022-07-09 22:14:03 +0530

    audio player added

--- app/src/main/java/com/example/sqlpro/MainActivity2.java
+++ app/src/main/java/com/example/sqlpro/MainActivity2.java
@@ -2,91 +2,92 @@ package com.example.sqlpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    EditText updateold,updatenew,delete;
-    dbAdapter helper;
+    TextView songtitle ;
+    Button play, forward, rewind, pause, stop, reset;
+    MediaPlayer mediaPlayer ;
+    int starttime = 0 ;
+    int stopttime = 0;
+    int forwardtime = 5000 ;
+    int backwardtime = 5000 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        updateold=findViewById(R.id.editTextTextEmailAddress5);
-        updatenew=findViewById(R.id.editTextTextEmailAddress6);
-        delete=findViewById(R.id.editTextText3);
+        songtitle = findViewById(R.id.songname) ;
+        play = findViewById(R.id.play) ;
+        forward = findViewById(R.id.forward) ;
+        rewind = findViewById(R.id.rewind) ;
+        pause = findViewById(R.id.pause) ;
+        stop = findViewById(R.id.stop) ;
+        reset = findViewById(R.id.restart) ;
+        mediaPlayer = MediaPlayer.create(this, R.raw.first) ;
+        songtitle.setText("Magnolia.mp4");
 
-        helper=new dbAdapter(this);
-
-
-    }
-
-    public void viewdata(View view)
-    {
-        String data = helper.getData();
-//        Message.message(this,data);
-        if(data.length()>0)
-        {
-            Toast.makeText(this, data, Toast.LENGTH_LONG).show();
-        }
-        else
-        {
-            Toast.makeText(this, "Nothing to show !!", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public void update( View view)
-    {
-        String u1 = updateold.getText().toString();
-        String u2 = updatenew.getText().toString();
-        if(u1.isEmpty() || u2.isEmpty())
-        {
-//            Message.message(getApplicationContext(),"Enter Data");
-            Toast.makeText(this, "Enter both the fields", Toast.LENGTH_LONG).show();
-        }
-        else
-        {
-            int a= helper.updateName( u1, u2);
-            if(a<=0)
-            {
-//                Message.message(getApplicationContext(),"Unsuccessful");
-                Toast.makeText(this, "Unsucessful", Toast.LENGTH_LONG).show();
-                updateold.setText("");
-                updatenew.setText("");
-            } else {
-//                Message.message(getApplicationContext(),"Updated");
-                Toast.makeText(this, "Updated Sucessfully", Toast.LENGTH_SHORT).show();
-                updateold.setText("");
-                updatenew.setText("");
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity2.this, "Playing Media now", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
             }
-        }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity2.this, "Pausing the Media", Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity2.this, "Media stopped", Toast.LENGTH_SHORT).show();
+                mediaPlayer.stop();
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.reset();
+                mediaPlayer.start();
+            }
+        });
+
+        forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int currentpos = mediaPlayer.getCurrentPosition() ;
+                if((currentpos+forwardtime) <= (stopttime = mediaPlayer.getDuration())){
+                    mediaPlayer.seekTo(currentpos+forwardtime);
+                }
+            }
+        });
+
+        rewind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int currentpos = mediaPlayer.getCurrentPosition() ;
+                if((currentpos-backwardtime) >= starttime){
+                    mediaPlayer.seekTo(currentpos-backwardtime);
+                }
+            }
+        });
+
+
 
     }
-    public void delete( View view)
-    {
-        String uname = delete.getText().toString();
-        if(uname.isEmpty())
-        {
-            Toast.makeText(this, "Enter data", Toast.LENGTH_LONG).show();
-        }
-        else{
-            int a= helper.delete(uname);
-            if(a<=0)
-            {
-                Toast.makeText(this, "Unsucessful", Toast.LENGTH_LONG).show();
-                delete.setText("");
-            }
-            else
-            {
-                Toast.makeText(this, "Deleted", Toast.LENGTH_LONG).show();
-                delete.setText("");
-            }
-        }
-    }
-
 }
